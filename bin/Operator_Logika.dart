d1e5e211b8e1d0f37fb67ababfd5d0893f3c18ec
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2020-11-11 19:07:28 +0700

    mengubah isi file operator logika

---
+++ bin/Operator_Logika.dart
@@ -0,0 +1,89 @@
+void main(){
+
+  /*
+    Operator logika adalah operator yang digunakan
+    untuk membandingkan 2 kondisi logika. yaiut logika
+    benar ( TRUE ) dan logika salah ( FALSE )
+   */
+
+  var a,b,c;
+
+  // OR  atau (||)
+  print('==== OR (||) =====');
+
+  a = false;
+  b = false;
+  c = (a||b);
+  print('$a || $b = $c');
+
+  a = false;
+  b = true;
+  c = (a||b);
+  print('$a || $b = $c');
+
+  a = true;
+  b = false;
+  c = (a||b);
+  print('$a || $b = $c');
+
+  a = true;
+  b = true;
+  c = (a||b);
+  print('$a || $b = $c \n');
+
+
+  // AND  atau (&&)
+  print('==== AND (&&) =====');
+
+  a = false;
+  b = false;
+  c = (a&&b);
+  print('$a && $b = $c');
+
+  a = false;
+  b = true;
+  c = (a&&b);
+  print('$a && $b = $c');
+
+  a = true;
+  b = false;
+  c = (a&&b);
+  print('$a && $b = $c');
+
+  a = true;
+  b = true;
+  c = (a&&b);
+  print('$a && $b = $c\n');
+
+  // XOR  atau (^)
+  print('==== XOR (^) =====');
+
+  a = false;
+  b = false;
+  c = (a^b);
+  print('$a ^ $b = $c');
+
+  a = false;
+  b = true;
+  c = (a^b);
+  print('$a ^ $b = $c');
+
+  a = true;
+  b = false;
+  c = (a^b);
+  print('$a ^ $b = $c');
+
+  a = true;
+  b = true;
+  c = (a^b);
+  print('$a ^ $b = $c\n');
+
+  // NEGASI ( ! )
+  a = true;
+  b = !a;
+  print('Negasi dari A, jika a nya true adalah $b');
+
+  a = false;
+  b = !a;
+  print('Negasi dari A, jika a nya false adalah $b');
+}
---
+++ test/Operator_Logika_test.dart
@@ -0,0 +1,8 @@
+import 'package:Operator_Logika/Operator_Logika.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
